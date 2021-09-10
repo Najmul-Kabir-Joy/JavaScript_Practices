718f67dec5c076aa3fda37d74a1a3913e120f2c7
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2021-09-11 00:24:06 +0600

    alert location

---
+++ Browser_Debug/browserApi/alert_confirm_prompt.js
@@ -0,0 +1,10 @@
+alert('by refreshing getting this alert');
+const alertz = () => {
+    alert('by pressing button');
+}
+const party = () => {
+    console.log(confirm('Party on or not?'));
+}
+const nameIn = () => {
+    console.log(prompt('Please enter your name.'));
+}
---
+++ Browser_Debug/browserApi/location.js
@@ -0,0 +1,11 @@
+const refreshPage = () => {
+    location.reload();
+}
+const redirectPage = () => {
+    location.assign('https://www.google.com/')
+}
+
+//location.hash to change the hashtag of the url 
+//location.search to see the query and '&' is used to combine two quries
+//location.host to see host name 
+//location.href to see the full url
