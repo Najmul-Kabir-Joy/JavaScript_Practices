286d01b983759354f68d9c66b10f3d33d80efbc3
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2021-09-11 13:13:01 +0600

    control browser history

---
+++ Browser_Debug/browserApi/history.js
@@ -0,0 +1,6 @@
+/*window.history.go(value);
+if value = 0; or blank; refresh the page
+if value = positive something; forward the page
+if value = negative something; backward the page
+if window.history.length = how many history is there. if 1 that means its for the current
+*/
